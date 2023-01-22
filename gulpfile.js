76e19ed729a3ab422f69ff893b3231cb587a0907
lukecode <lucassocorrosilva@gmail.com>
2023-01-22 19:40:29 -0300

    Projeot imoInnovate iníciado

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, watch, series } = require("gulp");
+
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+
+function css(done) {
+  src("src/less/app.less")
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"));
+
+  done();
+}
+
+function dev(done) {
+  watch("src/less/**/*.less", css);
+  done();
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.default = series(css);
+
