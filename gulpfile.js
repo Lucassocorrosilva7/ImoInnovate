1b61a3714b8d416617e3c37d52d90cc02cd759cb
lukecode <lucassocorrosilva@gmail.com>
2023-01-27 21:06:41 -0300

    Minificando imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const { src, dest, watch, series } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 
+// Css e Less
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
@@ -16,12 +17,54 @@ function css(done) {
   done();
 }
 
+// Imagens
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
+const paths = {
+  less: "src/less/**/*.less",
+  image: "src/images/**/*",
+};
+
+function image() {
+  return src(paths.image)
+    .pipe(cache(imagemin({ optimizationLevel: 3 })))
+    .pipe(dest("build/images"));
+}
+
+function versionWebp(done) {
+  const options = {
+    quality: 50,
+  };
+  src("src/images/**/*.{png,jpg}")
+    .pipe(webp(options))
+    .pipe(dest("build/images"));
+  done();
+}
+
+function versionAvif(done) {
+  const options = {
+    quality: 50,
+  };
+  src("src/images/**/*.{png,jpg}")
+    .pipe(avif(options))
+    .pipe(dest("build/images"));
+  done();
+}
+
 function dev(done) {
-  watch("src/less/**/*.less", css);
+  watch(paths.less, css);
+  watch(paths.image, image);
+  watch(paths.image, versionWebp);
+  watch(paths.image, versionAvif);
   done();
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css);
-
+exports.image = image;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(css, image, versionWebp, versionAvif, dev);
